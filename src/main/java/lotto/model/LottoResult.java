82f76fe58ac3a0b7a52138b4d9da3ecaa48a12da
eorb6502 <eorb6502@naver.com>
2026-02-09 16:44:51 +0900

    lotto result enum implemented

--- src/main/java/lotto/model/LottoResult.java
+++ src/main/java/lotto/model/LottoResult.java
@@ -1,9 +1,10 @@
-package lotto;
+package lotto.model;
 
 public enum LottoResult {
     RANK_FIRST,
     RANK_SECOND,
     RANK_THIRD,
     RANK_FOURTH,
-    RANK_FIFTH
+    RANK_FIFTH,
+    RANK_NONE,
 }
