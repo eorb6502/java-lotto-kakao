2f25b2f130922e2715b6e18dfd9616bdce54becb
eorb6502 <eorb6502@naver.com>
2026-02-09 16:55:15 +0900

    view implemented

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -1,4 +1,16 @@
 package lotto.view;
 
+import java.util.Scanner;
+
 public class InputView {
+    private final Scanner scanner;
+
+    public InputView() {
+        this.scanner = new Scanner(System.in);
+    }
+
+    public int inputPrice() {
+        System.out.println("구입금액을 입력해 주세요.");
+        return Integer.parseInt(scanner.nextLine().trim());
+    }
 }
--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -1,4 +1,28 @@
 package lotto.view;
 
+import lotto.model.LottoNumbers;
+import lotto.model.LottoResult;
+
+import java.util.List;
+import java.util.Map;
+
 public class OutputView {
+    public void printPurchasedLottos(List<LottoNumbers> purchasedLottos) {
+        System.out.println(purchasedLottos.size() + "개를 구매했습니다.");
+        for (LottoNumbers purchasedLotto : purchasedLottos) {
+            System.out.println(purchasedLotto.getNumbers());
+        }
+    }
+
+    public void printStatistics(Map<LottoResult, Integer> resultCountByRank, double profitRate) {
+        System.out.println();
+        System.out.println("당첨 통계");
+        System.out.println("---------");
+        System.out.printf("3개 일치 (5000원)- %d개%n", resultCountByRank.getOrDefault(LottoResult.RANK_FIFTH, 0));
+        System.out.printf("4개 일치 (50000원)- %d개%n", resultCountByRank.getOrDefault(LottoResult.RANK_FOURTH, 0));
+        System.out.printf("5개 일치 (1500000원)- %d개%n", resultCountByRank.getOrDefault(LottoResult.RANK_THIRD, 0));
+        System.out.printf("5개 일치, 보너스 볼 일치(30000000원) - %d개%n", resultCountByRank.getOrDefault(LottoResult.RANK_SECOND, 0));
+        System.out.printf("6개 일치 (2000000000원)- %d개%n", resultCountByRank.getOrDefault(LottoResult.RANK_FIRST, 0));
+        System.out.printf("총 수익률은 %.2f입니다.%n", profitRate);
+    }
 }
