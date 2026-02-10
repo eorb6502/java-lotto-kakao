c0754ef540d6f81d0df5c2f1ac30f707303b8f8b
Deun Lee <deunlee@deunlee.com>
2026-02-10 12:07:32 +0900

    feat: fix lottonumber and lottogenerator

--- src/main/java/lotto/model/LottoNumber.java
+++ src/main/java/lotto/model/LottoNumber.java
@@ -1,18 +1,11 @@
 package lotto.model;
 
-public class LottoNumber {
+public record LottoNumber(int value) {
     private static final int MIN_NUMBER = 1;
     private static final int MAX_NUMBER = 45;
 
-    private final int value;
-
-    public LottoNumber(int value) {
+    public LottoNumber {
         validateRange(value);
-        this.value = value;
-    }
-
-    public int getValue() {
-        return value;
     }
 
     private void validateRange(int value) {
--- src/main/java/lotto/model/LottoNumberGenerator.java
+++ src/main/java/lotto/model/LottoNumberGenerator.java
@@ -6,9 +6,9 @@ import java.util.List;
 import java.util.Random;
 
 public class LottoNumberGenerator {
+    private static final int LOTTO_SIZE = 6;
     private static final int LOTTO_MIN_NUMBER = 1;
     private static final int LOTTO_MAX_NUMBER = 45;
-    private static final int LOTTO_SIZE = 6;
 
     private final Random random;
 
@@ -21,30 +21,27 @@ public class LottoNumberGenerator {
     }
 
     public LottoNumbers generate() {
-        List<Integer> candidates = createCandidates();
+        // 1 ~ 45 숫자 리스트를 생성하고 랜덤하게 섞기
+        final List<Integer> candidates = new ArrayList<>();
+        for (int number = LOTTO_MIN_NUMBER; number <= LOTTO_MAX_NUMBER; number++) {
+            candidates.add(number);
+        }
         Collections.shuffle(candidates, random);
 
-        List<Integer> generatedNumbers = new ArrayList<>(candidates.subList(0, LOTTO_SIZE));
-        Collections.sort(generatedNumbers);
-        return new LottoNumbers(generatedNumbers);
+        // 랜덤한 리스트의 앞 6개 숫자를 추출하고 정렬
+        List<Integer> generated = new ArrayList<>(candidates.subList(0, LOTTO_SIZE));
+        Collections.sort(generated);
+        return new LottoNumbers(generated);
     }
 
     public List<LottoNumbers> generate(int count) {
         if (count < 0) {
             throw new IllegalArgumentException("생성 개수는 0 이상이어야 합니다.");
         }
-        List<LottoNumbers> generatedLottos = new ArrayList<>();
+        List<LottoNumbers> list = new ArrayList<>();
         for (int index = 0; index < count; index++) {
-            generatedLottos.add(generate());
+            list.add(generate());
         }
-        return generatedLottos;
-    }
-
-    private List<Integer> createCandidates() {
-        List<Integer> candidates = new ArrayList<>();
-        for (int number = LOTTO_MIN_NUMBER; number <= LOTTO_MAX_NUMBER; number++) {
-            candidates.add(number);
-        }
-        return candidates;
+        return list;
     }
 }
