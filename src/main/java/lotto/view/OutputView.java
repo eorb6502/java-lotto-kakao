e3f3050d7bf9cfc48a9144131e50eee144c086b5
eorb6502 <eorb6502@naver.com>
2026-02-12 10:36:14 +0900

    remove for expression with map:

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -8,6 +8,20 @@ import java.util.List;
 import java.util.Map;
 
 public class OutputView {
+    private static final List<LottoResult> STATISTICS_ORDER = List.of(
+        LottoResult.RANK_FIFTH,
+        LottoResult.RANK_FOURTH,
+        LottoResult.RANK_THIRD,
+        LottoResult.RANK_SECOND,
+        LottoResult.RANK_FIRST
+    );
+    private static final Map<LottoResult, String> STATISTICS_FORMAT = Map.of(
+        LottoResult.RANK_FIFTH, "3개 일치 (%s)- %d개%n",
+        LottoResult.RANK_FOURTH, "4개 일치 (%s)- %d개%n",
+        LottoResult.RANK_THIRD, "5개 일치 (%s)- %d개%n",
+        LottoResult.RANK_SECOND, "5개 일치, 보너스 볼 일치(%s) - %d개%n",
+        LottoResult.RANK_FIRST, "6개 일치 (%s)- %d개%n"
+    );
 
     public void printPurchasedLottos(final PurchaseResult result) {
         System.out.println("수동으로 " + result.manualCount() + "장, 자동으로 " + result.autoCount() + "개를 구매했습니다.");
@@ -21,21 +35,13 @@ public class OutputView {
         System.out.println();
         System.out.println("당첨 통계");
         System.out.println("---------");
-        System.out.printf("3개 일치 (%s)- %d개%n",
-            formatPrize(LottoResult.RANK_FIFTH.getPrize()),
-            resultCountByRank.getOrDefault(LottoResult.RANK_FIFTH, 0));
-        System.out.printf("4개 일치 (%s)- %d개%n",
-            formatPrize(LottoResult.RANK_FOURTH.getPrize()),
-            resultCountByRank.getOrDefault(LottoResult.RANK_FOURTH, 0));
-        System.out.printf("5개 일치 (%s)- %d개%n",
-            formatPrize(LottoResult.RANK_THIRD.getPrize()),
-            resultCountByRank.getOrDefault(LottoResult.RANK_THIRD, 0));
-        System.out.printf("5개 일치, 보너스 볼 일치(%s) - %d개%n",
-            formatPrize(LottoResult.RANK_SECOND.getPrize()),
-            resultCountByRank.getOrDefault(LottoResult.RANK_SECOND, 0));
-        System.out.printf("6개 일치 (%s)- %d개%n",
-            formatPrize(LottoResult.RANK_FIRST.getPrize()),
-            resultCountByRank.getOrDefault(LottoResult.RANK_FIRST, 0));
+        for (LottoResult lottoResult : STATISTICS_ORDER) {
+            System.out.printf(
+                STATISTICS_FORMAT.get(lottoResult),
+                formatPrize(lottoResult.getPrize()),
+                resultCountByRank.getOrDefault(lottoResult, 0)
+            );
+        }
         System.out.printf("총 수익률은 %.2f입니다.%n", profitRate);
     }
 
